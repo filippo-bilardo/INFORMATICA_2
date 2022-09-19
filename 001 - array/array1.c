529df50888e58147d669a25f48205975b3f8b07f
Filippo <filippo.bilardo@tiscali.it>
2022-09-19 11:27:05 +0200

    Create array1.c

---
+++ 001 - array/array1.c
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a[10];
+  
+  //per memorizzare
+  for(int i=0;i<10;i++)
+  {
+      a[i]=i;
+  }
+  
+  //per scrivere quanto memorizzato
+  for(int i=0;i<10;i++)
+  {
+      cout<<a[i]<<" ";
+  } 
+}
