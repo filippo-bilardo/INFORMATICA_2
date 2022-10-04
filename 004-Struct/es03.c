2d7ff7cd81d88a611f61549425ec4ae8c205560f
Filippo <filippo.bilardo@tiscali.it>
2022-10-04 17:58:53 +0000

    update

---
+++ 004-Struct/es03.c
@@ -0,0 +1,26 @@
+/**
+ * Ricorda che le stringhe in C sono in realtà una matrice di caratteri e, 
+ * sfortunatamente, non puoi assegnare un valore a una matrice come questa
+ */
+#include <stdio.h>
+#include <string.h>
+
+struct myStructure {
+  int myNum;
+  char myLetter;
+  char myString[30];  // String
+};
+
+int main() {
+  struct myStructure s1;
+
+  // Trying to assign a value to the string
+  //s1.myString = "Some text";  //Error
+  // Assign a value to the string using the strcpy function
+  strcpy(s1.myString, "Some text");
+
+  // Trying to print the value
+  printf("My string: %s\n", s1.myString);
+
+  return 0;
+}
