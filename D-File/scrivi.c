86cb343869ac170374130bf8d4418a71a2868242
Filippo <filippo.bilardo@tiscali.it>
2022-11-07 18:05:29 +0100

    Create scrivi.c

---
+++ D-File/scrivi.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main() {
+    FILE* fp;
+    char s[256], nome[] = "out.txt";
+    if((fp = fopen(nome, "w")) == NULL) {
+        fprintf(stderr, "Errore nell'apertura di %s\n", nome);
+        exit(1);
+    }
+
+    do {
+        scanf("%s", s);
+        fprintf(fp, "%s\n", s);
+    } while(strcmp(s, "fine") != 0);
+
+    fclose(fp);
+}
+
