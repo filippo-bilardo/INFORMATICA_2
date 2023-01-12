2ecf3ea894036ce26753f319a6bf5bf35ce7cde4
Filippo <filippo.bilardo@tiscali.it>
2023-01-12 08:57:13 +0000

    esempio Frazione.cpp

---
+++ E-OOP/Frazione.cpp
@@ -0,0 +1,73 @@
+// Direttive di preprocessore
+#include <iostream> //cout, cin
+
+using namespace std; // Per le funzioni cout e cin
+
+// Definizione della classe
+class Frazione {
+private:
+	// Definizione degli attributi di classe
+	int numeratore;
+	int denominatore;
+
+public:
+	// Costruttore di default
+	Frazione() {}
+
+	// Metodi setters
+	void setNumeratore(int n) {
+		numeratore = n;
+	}
+
+	void setDenominatore(int n) {
+		denominatore = n;
+	}
+
+	// Metodi getters
+	int getNumeratore() {
+		return numeratore;
+	}
+
+	int getDenominatore() {
+		return denominatore;
+	}
+
+	// Definizione dei metodi
+	void stampa() {  // Metodo che stampa a video la frazione
+		/* quando l'invocazione di un metodo è all'interno della  
+		classe non usiamo la dot notation (spiegata in seguito) */
+		semplifica();
+		cout << numeratore << "/" << denominatore << endl;
+	}
+
+	void semplifica() { // Metodo che semplifica gli argomenti della frazione
+		int min;
+		min = MCD(numeratore, denominatore);
+		numeratore = numeratore / min;
+		denominatore = denominatore / min;
+	}
+
+	int MCD(int a, int b) { // Metodo che calcola il MCD (algoritmo di Euclide)
+		int c, resto;
+		if (b > a) {
+			c = a;
+			a = b;
+			b = c;
+		}
+		do {
+			resto = a % b;
+			a = b;
+			b = resto;
+		} while (resto != 0);
+		return a;
+	}
+};
+
+int main() { // Funzione principale
+	Frazione f1; 			// Definizione di un oggetto di classe Frazione
+	//f1.numeratore=5;   	// ERRATA perché numeratore è private
+	f1.setNumeratore(12); 	// Chiamata del metodo setNumeratore sull'oggetto
+	f1.setDenominatore(3);
+	f1.stampa(); 	// Semplificazione automatica (stampa chiama semplifica: f1 vale 4)
+	return 0;
+}
