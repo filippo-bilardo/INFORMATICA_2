4aabf9ed6310c05eb3c4c8a2c518a9c747fc5ab7
Filippo <filippo.bilardo@tiscali.it>
2023-02-28 02:33:54 +0100

    Update puls.cpp

--- E-OOP/Pulsante1/puls.cpp
+++ E-OOP/Pulsante1/puls.cpp
@@ -1,59 +1,5 @@
 #include "Pulsante.h"
 
-Pulsante::Pulsante(int pin) {
-  pin_ = pin;
-  statoPrecedente_ = LOW;
-  statoAttuale_ = LOW;
-  ultimoClick_ = 0;
-  ultimoDoppioClick_ = 0;
-  pinMode(pin_, INPUT_PULLUP);
-}
-
-bool Pulsante::press() {
-  statoAttuale_ = digitalRead(pin_);
-  if (statoAttuale_ == LOW && statoPrecedente_ == HIGH) {
-    statoPrecedente_ = statoAttuale_;
-    return true;
-  }
-  statoPrecedente_ = statoAttuale_;
-  return false;
-}
-
-bool Pulsante::click() {
-  if (press() && (millis() - ultimoClick_ > 50)) {
-    ultimoClick_ = millis();
-    return true;
-  }
-  return false;
-}
-
-bool Pulsante::doppio_click() {
-  if (click() && (millis() - ultimoDoppioClick_ < 500)) {
-    ultimoDoppioClick_ = millis();
-    return true;
-  }
-  ultimoDoppioClick_ = millis();
-  return false;
-}
-
-void Pulsante::test(int numero_test) {
-  switch (numero_test) {
-    case 1: 
-      // Test press
-      if(press()){
-        Serial.println("Pulsante premuto");
-      }
-      break;
-    case 2:
-      // Test click
-      if(click()){
-        Serial.println("Click");
-      }
-      break;
-    case 3:
-      // Test doppio_click
-      if(doppio_click()){#include "Pulsante.h"
-
 /**
 * @brief Costruttore della classe Pulsante
 * @param pin Pin del pulsante su Arduino
